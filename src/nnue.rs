f732af79ce09edbfa179196e3a3f00ecbe358a5b
Liam <lmedley092@gmail.com>
2026-06-14 02:02:50 -0500

    Revert HEAD to v0.6.0/net2 (bench 26242) pending netsf SPRT

--- src/nnue.rs
+++ src/nnue.rs
@@ -18,12 +18,12 @@ use crate::eval::Score;
 use crate::position::Position;
 use crate::types::{Color, PieceType, Square};
 
-const HIDDEN: usize = 768;
+const HIDDEN: usize = 512;
 const QA: i32 = 255;
 const QB: i32 = 64;
 const SCALE: i32 = 400;
 
-const NET_BYTES: &[u8] = include_bytes!("../nets/razorsf.nnue");
+const NET_BYTES: &[u8] = include_bytes!("../nets/razor2.nnue");
 
 pub struct Network {
     feature_weights: Box<[[i16; HIDDEN]; 768]>,
